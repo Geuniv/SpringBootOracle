9a917166349f280c90a17fa916ff4f92e31469c4
Geuniv <kkoowl99@gmail.com>
2023-08-24 13:30:54 +0900

    2023.08.24 스프링 강의 ( 2학기 )

--- src/main/java/kopo/poly/SpringBootOracleApplication.java
+++ src/main/java/kopo/poly/SpringBootOracleApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootOracleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootOracleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOracleApplication.class, args);
+    }
 
 }
