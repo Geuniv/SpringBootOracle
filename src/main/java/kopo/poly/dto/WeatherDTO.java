c4fc0ed4cc48941df30a4f012014a0016477bd2f
Geuniv <kkoowl99@gmail.com>
2023-11-21 23:45:41 +0900

    2023.11.21 스프링 강의 ( 날씨 정보 크롤링 복구 )

--- src/main/java/kopo/poly/dto/WeatherDTO.java
+++ src/main/java/kopo/poly/dto/WeatherDTO.java
@@ -1,16 +1,17 @@
 package kopo.poly.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
+
+import java.util.List;
 
 @Getter
 @Setter
-@ToString
 public class WeatherDTO {
 
     private String collectTime; // 수집시간
-    
+
     private String seq; // 수집된 데이터 순번
 
     private String region; // 지역
@@ -19,7 +20,7 @@ public class WeatherDTO {
 
     private String weather; // 현재 날씨
 
-    
+
     // 아래에 있는 등록자 or 등록일자와 수정자 or 수정일자는 굳이 넣을 필요는 없지만 그냥 추가함
     private String regId;
 
@@ -28,4 +29,5 @@ public class WeatherDTO {
     private String chgId;
 
     private String chgDt;
+
 }
